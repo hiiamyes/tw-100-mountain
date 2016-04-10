a3c135eae73d1a1e9eb45701964403aaf83ff8a7
Yes <joshuayes@gmail.com>
2016-04-10 23:13:54 +0800

    add markers for 100 mountains

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,77 @@
-import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/fuelSavingsActions';
+import L from 'leaflet';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const App = (props) => {
-  return (
-    <div>
-      <IndexLink to="/">Home</IndexLink> | <Link to="/About">About</Link>
-      <br/>
-      {props.children}
-    </div>
-  );
-};
+class App extends Component {
+
+  componentDidMount(){
+    var mountains = require('../assets/mountains.json');
+
+    var map = L.map('map').setView([mountains[0].latitude.value, mountains[0].longitude.value], 8);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    setTimeout( () => {
+      map.invalidateSize();
+    }, 10)
+
+    // setTimeout(function(){map.invalidateSize();            map.setView([5,-20],3);},1000)
+
+
+    let markers = mountains.map( mountain => {
+      return (
+        L.marker([mountain.latitude.value, mountain.longitude.value]).addTo(map).bindPopup(mountain.name)
+          .on('click', (e) => {
+            map.setView(e.latlng);
+          })
+      );
+    })
+    markers[0].openPopup();
+  }
+  render() {
+    const position = [51.505, -0.09];
+
+    return (
+      <div id='map'>
+
+      </div>
+    );
+  }
+}
+
+// {/*<Map center={position} zoom={13}>
+//   <TileLayer
+//     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+//     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//   />
+//   {/*<Marker position={position}>
+//     <Popup>
+//       <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+//     </Popup>
+//   </Marker>*/}
+// </Map>*/}
 
 App.propTypes = {
-  children: PropTypes.element
+  actions: PropTypes.object.isRequired,
+  appState: PropTypes.object.isRequired
 };
 
-export default App;
+function mapStateToProps(state) {
+  return {
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/reducers/fuelSavings.js
+++ src/reducers/fuelSavings.js
@@ -1,6 +1,4 @@
 import {SAVE_FUEL_SAVINGS, CALCULATE_FUEL_SAVINGS} from '../constants/ActionTypes';
-import calculator from '../businessLogic/fuelSavingsCalculator';
-import dateHelper from '../businessLogic/dateHelper';
 import objectAssign from 'object-assign';
 
 const initialState = {
@@ -27,26 +25,7 @@ const initialState = {
 //and update values on the copy.
 export default function fuelSavingsAppState(state = initialState, action) {
 	switch (action.type) {
-		case SAVE_FUEL_SAVINGS:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
-			return objectAssign({}, state, { dateModified: dateHelper.getFormattedDateTime(new Date()) });
-
-		case CALCULATE_FUEL_SAVINGS:
-    { // limit scope with this code block, to satisfy eslint no-case-declarations rule.
-      let newState = objectAssign({}, state);
-      newState[action.fieldName] = action.value;
-      let calc = calculator();
-      newState.necessaryDataIsProvidedToCalculateSavings = calc.necessaryDataIsProvidedToCalculateSavings(newState);
-      newState.dateModified = dateHelper.getFormattedDateTime(new Date());
-
-      if (newState.necessaryDataIsProvidedToCalculateSavings) {
-        newState.savings = calc.calculateSavings(newState);
-      }
-
-      return newState;
-    }
-
+	
 		default:
 			return state;
 	}
