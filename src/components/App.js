92dada1dbd270f1cac44116a4660c42bff15b5ce
Yes <joshuayes@gmail.com>
2016-04-10 23:47:08 +0800

    adjust route for github page

--- src/components/App.js
+++ src/components/App.js
@@ -8,36 +8,32 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 class App extends Component {
 
   componentDidMount(){
-    // var mountains = require('../assets/mountains.json');
-    //
-    // var map = L.map('map').setView([mountains[0].latitude.value, mountains[0].longitude.value], 8);
-    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    //   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(map);
-    //
-    // setTimeout( () => {
-    //   map.invalidateSize();
-    // }, 10)
-    //
-    // // setTimeout(function(){map.invalidateSize();            map.setView([5,-20],3);},1000)
-    //
-    //
-    // let markers = mountains.map( mountain => {
-    //   return (
-    //     L.marker([mountain.latitude.value, mountain.longitude.value]).addTo(map).bindPopup(mountain.name)
-    //       .on('click', (e) => {
-    //         map.setView(e.latlng);
-    //       })
-    //   );
-    // })
-    // markers[0].openPopup();
+    var mountains = require('../assets/mountains.json');
+
+    var map = L.map('map').setView([mountains[0].latitude.value, mountains[0].longitude.value], 8);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    setTimeout( () => {
+      map.invalidateSize();
+    }, 10)
+
+    let markers = mountains.map( mountain => {
+      return (
+        L.marker([mountain.latitude.value, mountain.longitude.value]).addTo(map).bindPopup(mountain.name)
+          .on('click', (e) => {
+            map.setView(e.latlng);
+          })
+      );
+    })
+    markers[0].openPopup();
   }
   render() {
     const position = [51.505, -0.09];
 
     return (
       <div id='map'>
-        gg
       </div>
     );
   }
